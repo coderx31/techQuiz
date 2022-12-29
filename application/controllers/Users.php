732bf448ed8d931ea6c62a2cc07bdbabdd6d175d
= <=>
2022-12-29 15:59:04 +0530

    added some moditifications to user controller and model

--- application/controllers/Users.php
+++ application/controllers/Users.php
@@ -12,8 +12,12 @@
             $firstname = $this->post('firstname');
             $lastname = $this->post('lastname');
             $username = $this->post('username');
+            $email = $this->post('email');
             $password = $this->post('password');
             $password2 = $this->post('password2');
+
+            $isUsernameTaken = $this->user_model->check_username_exists($username);
+            $isEmailTaken = $this->user_model->check_email_exists($email);
 
             if (!$firstname || !$lastname || !$username || !$password || !$password2) {
                 $this->response([
@@ -27,17 +31,30 @@
                     'error' => 'passwords didn\'t match',
                     'result' => null
                 ], 400);
+            } else if ($isUsernameTaken == true || $isEmailTaken == true) {
+                $this->response([
+                    'code' => -1,
+                    'error' => 'given username/email is already taken. choose another one',
+                    'result' => null
+                ], 400);
             } else {
                 $data = array(
                     'user_id' => Uuid::uuid1(),
                     'firstname' => $firstname,
                     'lastname' => $lastname,
                     'username' => $username,
-                    'password' => 'encrypted password'
+                    'email' => $email,
+                    'password' => password_hash($password, PASSWORD_BCRYPT, array('salt' => $this->config->item('password_salt')))
                 );
                 // encrypts the password
                 // save to database
+                $result = $this->user_model->register($data);
                 // return the response
+                $this->response([
+                    'code' => 0,
+                    'error' => null,
+                    'result' => $result
+                ], 200);
             }
         }
 
@@ -53,14 +70,46 @@
                     'result' => null
                 ],400);
             }
+            $result = $this->user_model->login(array('username' => $username, 'password' => $password));
 
             // check the password validation
-            // return the response - api key or something
+            if(!$result) {
+                // login failed
+                $this->response([
+                    'code' => -1,
+                    'error' => 'loggin failed',
+                    'result' => null
+                ],400);
+            } else {
+                // create the session
+                $user_data = array(
+                    'user_id' => $result['user_id'],
+                    'username' => $result['username'],
+                    'logged_in' => true
+                );
+                $this->session->set_userdata($user_data);
+                // return the response
+                $this-> response([
+                    'code' => 0,
+                    'error' => null,
+                    'result' => true
+                ],200);
+            }
         }
 
         // user logout
         public function logout_post() {
             // to be implemented
+            // unset the session
+            $this->session->unset_userdata('logged_in');
+            $this->session->unset_userdata('user_id');
+            $this->session->unset_userdata('username');
+
+            $this->response([
+                'code' => 0,
+                'error' => null,
+                'result' => 'user logged out successfully'
+            ], 200);
         }
 
         public function isEmailAvailable_get() {
--- application/models/User_model.php
+++ application/models/User_model.php
@@ -7,17 +7,32 @@
 
         // insert user to database
         public function register($data) {
-            // to be implemented
+            return $this->db->insert('users', $data);
         }
 
         // user login
-        public function login() {
+        public function login($data) {
             // to be implemented
+            $this->db->where('username',$data['username']);
+            $this->db->where('password', $data['password']);
+
+            $result = $this->db->get('users');
+            if($result->num_rows() == 1) {
+                // verify the password
+                $isSuccess = password_verify($data['password'], $result->row(0)->password);
+                if ($isSuccess) {
+                    // return username and password
+                    return array('user_id' => $result->row(0)->user_id, 'username' => $result->row(0)->username);
+                } else {
+                    return false;
+                }
+            } else {
+                return false;
+            }
         }
 
         // check email availability
         public function check_email_exists($email) {
-            // to be implemented
             $query = $this->db->get_where('users', array( 'email' => $email ));
             if(empty($query->row_array())) {
                 return true;
