65f719f16577898bb784cc91b308ae4b1d0b9e5a
= <=>
2023-01-22 11:33:49 +0530

    config file updated with index page

--- application/config/config.php
+++ application/config/config.php
@@ -35,7 +35,7 @@ $config['base_url'] = 'https://w1761547.users.ecs.westminster.ac.uk/techQuiz';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = '';
+$config['index_page'] = 'index.php';
 
 /*
 |--------------------------------------------------------------------------
